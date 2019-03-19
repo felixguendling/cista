8cc65cc4986fc548008e05e6440899e530e80885
Felix Guendling <felixguendling@gmail.com>
2019-03-19 12:36:08 +0100

    credits

--- include/cista/reflection/arity.h
+++ include/cista/reflection/arity.h
@@ -2,6 +2,16 @@
 
 #include <type_traits>
 
+// Credits: Implementation by ChemiaAion
+// https://gist.github.com/ChemiaAion/4118a3598f0b120b7f9c5884e9799a8b
+//
+// Resources:
+// https://playfulprogramming.blogspot.com/2016/12/serializing-structs-with-c17-structured.html
+// https://codereview.stackexchange.com/questions/142804/get-n-th-data-member-of-a-struct
+// https://stackoverflow.com/questions/39768517/structured-bindings-width
+// https://stackoverflow.com/questions/35463646/arity-of-aggregate-in-logarithmic-time
+// https://stackoverflow.com/questions/38393302/returning-variadic-aggregates-struct-and-syntax-for-c17-variadic-template-c
+
 namespace cista {
 
 struct instance {
