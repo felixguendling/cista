c5ba923ebcc1aedf1ee2d92ea56a7b9ce2865198
Felix Guendling <felixguendling@gmail.com>
2019-03-31 17:14:17 +0200

    hash: structure test

--- test/hash_test.cc
+++ test/hash_test.cc
@@ -2,11 +2,28 @@
 
 #include "cista.h"
 
-struct test {
-  int i;
-  int* j;
-};
+TEST_CASE("hash test") {
+  struct test {
+    int i;
+    int* j;
+  };
+
+  CHECK(cista::type_hash<int>() != cista::type_hash<test>());
+}
 
 TEST_CASE("hash test") {
-  CHECK(cista::type_hash<int>() != cista::type_hash<test>());
+  struct s1 {
+    int i;
+    struct {
+      int j;
+    } k;
+  };
+  struct s2 {
+    struct {
+      int i;
+    } j;
+    int k;
+  };
+
+  CHECK(cista::type_hash<s1>() != cista::type_hash<s2>());
 }
