5a0dfe4fa828c55b02369bb1da84c5896f268256
Felix Gündling <felixguendling@gmail.com>
2019-04-08 10:52:17 +0200

    serialized_size(void* param = nullptr) (#23), closes #22

--- include/cista/serialized_size.h
+++ include/cista/serialized_size.h
@@ -7,7 +7,8 @@
 namespace cista {
 
 template <typename T>
-static inline constexpr size_t serialized_size() {
+static inline constexpr size_t serialized_size(void* param = nullptr) {
+  (void)param;
   return sizeof(decay_t<T>);
 }
 
