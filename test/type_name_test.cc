c93219064c8bcf30b32132689838a53923e68190
Felix Guendling <felixguendling@gmail.com>
2019-04-08 10:53:53 +0200

    wip

--- test/type_name_test.cc
+++ test/type_name_test.cc
@@ -1,12 +1,7 @@
 constexpr auto msvc =
-    R("struct cista::basic_vector<struct cista::basic_unique_ptr<struct "
-      "graphns::offset::node,struct cista::offset_ptr<struct "
-      "graphns::offset::node> >,struct cista::offset_ptr<struct "
-      "cista::basic_unique_ptr<struct graphns::offset::node,struct "
-      "cista::offset_ptr<struct graphns::offset::node> > >,unsigned int>");
+    R"(struct cista::basic_vector<struct cista::basic_unique_ptr<struct graphns::offset::node,struct cista::offset_ptr<struct graphns::offset::node> >,struct cista::offset_ptr<struct cista::basic_unique_ptr<struct graphns::offset::node,struct cista::offset_ptr<struct graphns::offset::node> > >,unsigned int>)";
 
 constexpr auto clang =
-    R("cista::basic_vector<cista::basic_unique_ptr<graphns::offset::edge, "
-      "cista::offset_ptr<graphns::offset::edge> >, "
-      "cista::offset_ptr<cista::basic_unique_ptr<graphns::offset::edge, "
-      "cista::offset_ptr<graphns::offset::edge> > >, unsigned int>");
+    R"(cista::basic_vector<cista::basic_unique_ptr<graphns::offset::node, cista::offset_ptr<graphns::offset::node> >, cista::offset_ptr<cista::basic_unique_ptr<graphns::offset::node, cista::offset_ptr<graphns::offset::node> > >, unsigned int>)";
+
+// constexpr gcc =
