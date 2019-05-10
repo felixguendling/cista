95a9a30d53fa219a34a0e0841212e7e2f847abd1
Felix Guendling <felixguendling@gmail.com>
2019-05-10 15:35:39 +0200

    add windows.h include to file.h on MSVC

--- include/cista/targets/file.h
+++ include/cista/targets/file.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#ifdef _MSC_VER
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <io.h>
+#include <windows.h>
+#endif
+
 #include <cinttypes>
 #include <memory>
 
