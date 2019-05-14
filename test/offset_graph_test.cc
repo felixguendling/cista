35b74e7345c5186c705c543967ac1c2e98b75602
Felix Guendling <felixguendling@gmail.com>
2019-05-14 15:49:16 +0200

    offset file test mode: cista::mode::WITH_INTEGRITY

--- test/offset_graph_test.cc
+++ test/offset_graph_test.cc
@@ -85,7 +85,8 @@ inline std::set<node const*> bfs(node const* entry) {
 
 TEST_CASE("graph offset serialize file") {
   constexpr auto const FILENAME = "offset_graph.bin";
-  constexpr auto const EXPECTED_BUF_CHECKSUM = 15515468028119767496ULL;
+  constexpr auto const EXPECTED_BUF_CHECKSUM = 10235660601222901724ULL;
+  constexpr auto const MODE = cista::mode::WITH_INTEGRITY;
 
   std::remove(FILENAME);
 
@@ -105,7 +106,7 @@ TEST_CASE("graph offset serialize file") {
     n3->add_edge(e3);
 
     cista::file f{FILENAME, "w+"};
-    cista::serialize(f, g);
+    cista::serialize<MODE>(f, g);
 
     CHECK(f.checksum() == EXPECTED_BUF_CHECKSUM);
   }  // EOL graph
@@ -118,7 +119,7 @@ TEST_CASE("graph offset serialize file") {
   auto b = cista::file(FILENAME, "r").content();
   CHECK(cista::hash(b) == EXPECTED_BUF_CHECKSUM);
 
-  auto const g = data::deserialize<graph>(b);
+  auto const g = data::deserialize<graph, MODE>(b);
   auto const visited = bfs(g->nodes_[0].get());
   unsigned i = 0;
   CHECK((*std::next(begin(visited), i++))->name_ == data::string{"NODE A"});
