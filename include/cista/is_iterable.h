b41a5c01c221b117fb4f8b18eb99fafc7207e371
Felix Gündling <felix.guendling@gmail.com>
2019-09-09 16:34:02 +0200

    is_comparable(_v) and it_value(_t) trait utilities

--- include/cista/is_iterable.h
+++ include/cista/is_iterable.h
@@ -3,6 +3,8 @@
 #include <iterator>
 #include <type_traits>
 
+#include "cista/decay.h"
+
 namespace cista {
 namespace detail {
 
@@ -17,9 +19,24 @@ struct is_iterable<T, std::void_t<decltype(begin(std::declval<T>())),
                                   decltype(end(std::declval<T>()))>>
     : std::true_type {};
 
-}  // namespace detail
+template <typename T, typename = void>
+struct it_value {
+  using type = void;
+};
 
 template <typename T>
-constexpr bool is_iterable_v = detail::is_iterable<T>::value;
+struct it_value<T, std::enable_if_t<is_iterable<T>::value>> {
+  using type = decay_t<decltype(*begin(std::declval<T>()))>;
+};
+
+}  // namespace detail
+
+using detail::is_iterable;
+
+template <typename T>
+constexpr bool is_iterable_v = is_iterable<T>::value;
+
+template <typename T>
+using it_value_t = typename detail::it_value<T>::type;
 
 }  // namespace cista
