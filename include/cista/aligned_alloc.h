945a609dd3df0f3340e5f07fc2ccc33dfe8afde3
Felix Guendling <felixguendling@gmail.com>
2019-10-18 18:36:24 +0200

    CISTA_ALIGNED_ALLOC: check _LIBCPP_HAS_C11_FEATURES and _GLIBCXX_HAVE_ALIGNED_ALLOC

--- include/cista/aligned_alloc.h
+++ include/cista/aligned_alloc.h
@@ -18,15 +18,15 @@ T to_next_multiple(T const n, T const multiple) {
 #define CISTA_ALIGNED_ALLOC(alignment, size) \
   (_aligned_malloc((size), cista::next_power_of_two((alignment))))
 #define CISTA_ALIGNED_FREE(ptr) (_aligned_free((ptr)))
-#elif defined(__APPLE__)
-#include <cstdlib>
-#define CISTA_ALIGNED_ALLOC(alignment, size) (std::malloc((size)))
-#define CISTA_ALIGNED_FREE(ptr) (std::free((ptr)))
-#else
+#elif defined(_LIBCPP_HAS_C11_FEATURES) || defined(_GLIBCXX_HAVE_ALIGNED_ALLOC)
 #include <memory>
 #define CISTA_ALIGNED_ALLOC(alignment, size) \
   (std::aligned_alloc(                       \
       cista::next_power_of_two((alignment)), \
       cista::to_next_multiple((size), cista::next_power_of_two((alignment)))))
 #define CISTA_ALIGNED_FREE(ptr) std::free((ptr))
+#else
+#include <cstdlib>
+#define CISTA_ALIGNED_ALLOC(alignment, size) (std::malloc((size)))
+#define CISTA_ALIGNED_FREE(ptr) (std::free((ptr)))
 #endif
