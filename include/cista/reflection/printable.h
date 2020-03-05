4ffe216c106845beb7bbd7a69e5dc4c08cf2d262
Neargye <neargye@gmail.com>
2020-03-05 20:47:01 +0500

    fix #64

--- include/cista/reflection/printable.h
+++ include/cista/reflection/printable.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ostream>
+#include <type_traits>
 
 #ifndef CISTA_PRINTABLE_NO_VEC
 #include <vector>
@@ -8,9 +9,23 @@
 
 #include "cista/reflection/for_each_field.h"
 
+namespace cista {
+
+namespace enum_ostream_operators {
+
+template <typename E, typename = std::enable_if_t<std::is_enum_v<std::decay_t<E>>>>
+std::ostream& operator<<(std::ostream& out, E value) {
+  return out << static_cast<std::underlying_type_t<std::decay_t<E>>>(value);
+}
+
+}  // namespace cista::enum_ostream_operators
+
+}  // namespace cista
+
 #ifndef CISTA_PRINTABLE_NO_VEC
 template <typename T>
 inline std::ostream& operator<<(std::ostream& out, std::vector<T> const& v) {
+  using namespace ::cista::enum_ostream_operators;
   out << "[\n  ";
   auto first = true;
   for (auto const& e : v) {
@@ -28,7 +43,8 @@ inline std::ostream& operator<<(std::ostream& out, std::vector<T> const& v) {
   friend std::ostream& operator<<(std::ostream& out, class_name const& o) { \
     bool first = true;                                                      \
     out << "{";                                                             \
-    cista::for_each_field(o, [&](auto&& f) {                                \
+    ::cista::for_each_field(o, [&](auto&& f) {                              \
+      using namespace ::cista::enum_ostream_operators;                      \
       if (first) {                                                          \
         out << f;                                                           \
         first = false;                                                      \
