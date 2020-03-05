95b39cc32c271ac07b74af99b797c5c855dfaf78
Neargye <neargye@gmail.com>
2020-03-05 20:47:01 +0500

    fix #64

--- test/printable_test.cc
+++ test/printable_test.cc
@@ -8,17 +8,20 @@
 #include "cista/reflection/printable.h"
 #endif
 
+enum class Color : int { RED, BLUE, GREEN };
+
 struct a {
   CISTA_PRINTABLE(a)
   int i_ = 1;
   int j_ = 2;
   double d_ = 100.0;
   std::string s_ = "hello world";
+  Color c_ = Color::BLUE;
 };
 
 TEST_CASE("printable") {
   a instance;
   std::stringstream ss;
   ss << instance;
-  CHECK(ss.str() == "{1, 2, 100, hello world}");
+  CHECK(ss.str() == "{1, 2, 100, hello world, 1}");
 }
