474dc26ce5adc74afa01a2f18a62a19803cfbbb4
Felix Gündling <felix.guendling@gmail.com>
2020-05-09 17:31:57 +0200

    hashing.h: do not hash string with 0 length
    MSVC debug mode does not like dereferencing iterators &*ptr
    even if just taking the address of the element

--- include/cista/hashing.h
+++ include/cista/hashing.h
@@ -100,7 +100,9 @@ struct hashing {
     } else if constexpr (is_string_like_v<Type>) {
       using std::begin;
       using std::end;
-      return hash(std::string_view{&(*begin(el)), el.size()}, seed);
+      return el.size() == 0U
+                 ? seed
+                 : hash(std::string_view{&(*begin(el)), el.size()}, seed);
     } else if constexpr (std::is_scalar_v<Type>) {
       return hash_combine(seed, el);
     } else if constexpr (is_iterable_v<Type>) {
