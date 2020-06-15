cd02cd1806b08fd0707a8f3ff76393d3a396d9d9
Felix Guendling <felixguendling@gmail.com>
2020-06-15 19:50:59 +0200

    refactor member_index

--- include/cista/reflection/member_index.h
+++ include/cista/reflection/member_index.h
@@ -1,17 +1,18 @@
+#pragma once
+
 #include "cista/reflection/for_each_field.h"
 
 #include <type_traits>
 
 namespace cista {
 
-template <typename T, typename Interface>
-size_t member_index(T Interface::*const member_ptr) {
+template <typename T, typename MemberType>
+size_t member_index(MemberType T::*const member_ptr) {
   auto i = 0U, field_index = std::numeric_limits<unsigned>::max();
-  Interface interface{};
-  cista::for_each_field(interface, [&](auto&& m) {
-    if constexpr (std::is_same_v<decltype(&m),
-                                 decltype(&(interface.*member_ptr))>) {
-      if (&m == &(interface.*member_ptr)) {
+  T t{};
+  cista::for_each_field(t, [&](auto&& m) {
+    if constexpr (std::is_same_v<decltype(&m), decltype(&(t.*member_ptr))>) {
+      if (&m == &(t.*member_ptr)) {
         field_index = i;
       }
     }
