696abc72fb7a951e0bf4a7550f5491125e7a49ec
Felix Guendling <felixguendling@gmail.com>
2021-01-21 19:12:37 +0100

    remove debug output

--- include/cista/verify.h
+++ include/cista/verify.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include <stdexcept>
-#include <iostream>
 
 namespace cista {
 
 inline void verify(bool const condition, char const* msg) {
   if (!condition) {
-     std::cout << "Windows error: " << ::GetLastError() << std::endl;
     throw std::runtime_error(msg);
   }
 }
