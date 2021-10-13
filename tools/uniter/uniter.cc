951cb8d6fc060f3939c00f7fed6927fdc9ae7b5d
Felix Gündling <felix.guendling@gmail.com>
2021-10-13 09:40:56 +0200

    add license to single file version

--- tools/uniter/uniter.cc
+++ tools/uniter/uniter.cc
@@ -40,15 +40,26 @@ void write_file(std::string const& include_path, std::string const& path,
 }
 
 int main(int argc, char const** argv) {
-  if (argc < 3) {
-    printf("usage: %s include_path file0, [file1, [file2, ...]]\n", argv[0]);
+  if (argc < 4) {
+    printf("usage: %s license include_path file0, [file1, [file2, ...]]\n", argv[0]);
     return 1;
   }
 
-  auto const include_path = std::string{argv[1]};
-  std::set<std::string> included;
+  auto const license_path = std::string{argv[1]};
+  auto const include_path = std::string{argv[2]};
+
+  std::cout << "/*\n";
+  std::ifstream f{license_path.c_str()};
+  f.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  std::string line;
+  while (!f.eof() && f.peek() != EOF && std::getline(f, line)) {
+    std::cout << line << "\n";
+  }
+  std::cout << "*/\n\n";
+
   std::cout << "#pragma once\n\n";
-  for (int i = 2; i < argc; ++i) {
+  std::set<std::string> included;
+  for (int i = 3; i < argc; ++i) {
     write_file(include_path, argv[i], included);
   }
 }
