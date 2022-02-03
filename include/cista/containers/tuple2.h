d8bb2196ad0bd383e1cc43e6d5bf1ae504edc049
Julian Harbarth <harbarth@cs.tu-darmstadt.de>
2022-02-03 18:57:34 +0100

    remove unnecessary constructor calls

--- include/cista/containers/tuple2.h
+++ include/cista/containers/tuple2.h
@@ -37,7 +37,7 @@ struct max_size_of<> : std::integral_constant<std::size_t, 0> {};
 template <typename... Ts>
 constexpr auto max_size_of_v = max_size_of<Ts...>::value;
 
-template <std::size_t I, typename T>
+template <std::size_t I, typename... Ts>
 struct type_at_position;
 
 template <std::size_t I, typename T, typename... Ts>
@@ -46,6 +46,14 @@ struct type_at_position<I, tuple2<T, Ts...>>
 
 template <typename T, typename... Ts>
 struct type_at_position<0, tuple2<T, Ts...>> {
+  using type = T;
+};
+
+template <std::size_t I, typename T, typename... Ts>
+struct type_at_position<I, T, Ts...> : type_at_position<I - 1, Ts...> {};
+
+template <typename T, typename... Ts>
+struct type_at_position<0, T, Ts...> {
   using type = T;
 };
 
@@ -77,6 +85,17 @@ constexpr std::size_t get_size() {
   return get_offset<Ts...>(sizeof...(Ts) + 1);
 }
 
+template <std::size_t I, typename T, typename... Ts>
+constexpr typename std::enable_if_t<I == 0, T>&& get_arg(T&& arg, Ts&&...) {
+  return std::forward<T>(arg);
+}
+
+template <std::size_t I, typename T, typename... Ts>
+constexpr typename std::enable_if_t<I != 0, type_at_position_t<I, T, Ts...>>&&
+get_arg(T&& arg, Ts&&... args) {
+  return get_arg<I - 1>(std::forward<Ts>(args)...);
+}
+
 template <typename... Ts>
 struct alignas(max_align_of_v<Ts...>) tuple2 {
   template <std::size_t... Is>
@@ -85,33 +104,35 @@ struct alignas(max_align_of_v<Ts...>) tuple2 {
 
   tuple2() { default_construct(Indices); }
 
-  tuple2(tuple2 const& o) {
-    default_construct(Indices);
-    copy(o, Indices);
-  }
+  tuple2(tuple2 const& o) { copy_construct(o, Indices); }
 
   tuple2& operator=(tuple2 const& o) {
     if (&o != this) {
-      default_construct(Indices);
-      copy(o, Indices);
+      copy_assign(o, Indices);
     }
 
     return *this;
   }
 
-  tuple2(tuple2&& o) noexcept { move(o, Indices); }
+  tuple2(tuple2&& o) noexcept { move_construct(o, Indices); }
 
   tuple2& operator=(tuple2&& o) noexcept {
-    move(o, Indices);
+    move_assign(o, Indices);
     return *this;
   }
 
   tuple2(Ts&&... args) {  // NOLINT
-    default_construct(Indices);
-    set_array<std::tuple<Ts...>>(std::forward_as_tuple(args...), Indices);
+    move_construct_from_args(Indices, std::forward<Ts>(args)...);
   }
 
   ~tuple2() { destruct(Indices); }
+
+  template <std::size_t... Is>
+  constexpr void move_construct_from_args(seq_t<Is...>, Ts&&... args) {
+    (new (&get<Is>(*this)) type_at_position_t<Is, Ts...>(
+         std::move(get_arg<Is>(std::forward<Ts>(args)...))),
+     ...);
+  }
 
   template <typename Tuple, std::size_t... Is>
   constexpr void set_array(Tuple&& t, seq_t<Is...>) {
@@ -124,12 +145,25 @@ struct alignas(max_align_of_v<Ts...>) tuple2 {
   }
 
   template <typename From, std::size_t... Is>
-  constexpr void copy(From const& from, seq_t<Is...>) {
+  constexpr void copy_construct(From const& from, seq_t<Is...>) {
+    (new (&get<Is>(*this)) type_at_position_t<Is, tuple2<Ts...>>(get<Is>(from)),
+     ...);
+  }
+
+  template <typename From, std::size_t... Is>
+  constexpr void copy_assign(From const& from, seq_t<Is...>) {
     ((get<Is>(*this) = get<Is>(from)), ...);
   }
 
   template <typename From, std::size_t... Is>
-  constexpr void move(From&& from, seq_t<Is...>) {
+  constexpr void move_construct(From&& from, seq_t<Is...>) {
+    (new (&get<Is>(*this))
+         type_at_position_t<Is, tuple2<Ts...>>(std::move(get<Is>(from))),
+     ...);
+  }
+
+  template <typename From, std::size_t... Is>
+  constexpr void move_assign(From&& from, seq_t<Is...>) {
     ((get<Is>(*this) = std::move(get<Is>(from))), ...);
   }
 
@@ -157,26 +191,26 @@ tuple2(Head&& first, Tail&&... tail) -> tuple2<Head, Tail...>;
 
 template <size_t I, typename... Ts>
 auto& get(tuple2<Ts...>& t) {
-  using return_t = type_at_position_t<I, tuple2<Ts...>>;
+  using return_t = type_at_position_t<I, Ts...>;
   return *std::launder(reinterpret_cast<return_t*>(t.template get_ptr<I>()));
 }
 
 template <size_t I, typename... Ts>
 auto const& get(tuple2<Ts...> const& t) {
-  using return_t = type_at_position_t<I, tuple2<Ts...>>;
+  using return_t = type_at_position_t<I, Ts...>;
   return *std::launder(
       reinterpret_cast<return_t const*>(t.template get_ptr<I>()));
 }
 
 template <size_t I, typename... Ts>
 auto&& get(tuple2<Ts...>&& t) {
-  using return_t = type_at_position_t<I, tuple2<Ts...>>;
+  using return_t = type_at_position_t<I, Ts...>;
   return *std::launder(reinterpret_cast<return_t*>(t.template get_ptr<I>()));
 }
 
 template <size_t I, typename... Ts>
 auto const&& get(tuple2<Ts...> const&& t) {
-  using return_t = type_at_position_t<I, tuple2<Ts...>>;
+  using return_t = type_at_position_t<I, Ts...>;
   return *std::launder(
       reinterpret_cast<return_t const*>(t.template get_ptr<I>()));
 }
@@ -186,7 +220,7 @@ struct tuple_size<tuple2<Ts...>>
     : std::integral_constant<std::size_t, sizeof...(Ts)> {};
 
 template <std::size_t I, typename... Ts>
-struct tuple_element<I, tuple2<Ts...>> : type_at_position<I, tuple2<Ts...>> {};
+struct tuple_element<I, tuple2<Ts...>> : type_at_position<I, Ts...> {};
 
 template <typename... Ts>
 struct is_tuple<tuple2<Ts...>> : std::true_type {};
