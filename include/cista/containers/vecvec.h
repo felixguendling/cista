da08237274b1be49dd41cacc78a60539286d1d58
Felix Gündling <felix.guendling@gmail.com>
2022-08-09 19:19:05 +0200

    wip

--- include/cista/containers/vecvec.h
+++ include/cista/containers/vecvec.h
@@ -16,11 +16,21 @@ struct vecvec {
     using iterator = typename DataVec::iterator;
     using const_iterator = typename DataVec::iterator;
 
-    bucket(vecvec* map, size_t const i) : map_{map}, i_{i} {}
+    bucket(vecvec* map, index_value_type const i) : map_{map}, i_{to_idx(i)} {}
 
-    data_value_type& operator[](size_t const i) {
+    value_type& operator[](size_t const i) {
       assert(is_inside_bucket(i));
       return map_->data_[map_->bucket_starts_[i_] + i];
+    }
+
+    value_type const& at(size_t const i) const {
+      verify(i < size(), "bucket::at: index out of range");
+      return *(begin() + i);
+    }
+
+    value_type& at(size_t const i) {
+      verify(i < size(), "bucket::at: index out of range");
+      return *(begin() + i);
     }
 
     size_t size() const { return bucket_end_idx() - bucket_begin_idx(); }
@@ -42,9 +52,63 @@ struct vecvec {
     vecvec* map_;
   };
 
-  using value_type = bucket;
+  struct const_bucket final {
+    using value_type = data_value_type;
+    using iterator = typename DataVec::iterator;
+    using const_iterator = typename DataVec::const_iterator;
 
-  value_type operator[](size_t const i) { return {this, i}; }
+    const_bucket(vecvec const* map, index_value_type const i)
+        : map_{map}, i_{to_idx(i)} {}
+
+    value_type const& at(size_t const i) const {
+      verify(i < size(), "bucket::at: index out of range");
+      return *(begin() + i);
+    }
+
+    value_type& operator[](size_t const i) {
+      assert(is_inside_bucket(i));
+      return map_->data_[map_->bucket_starts_[i_] + i];
+    }
+
+    size_t size() const { return bucket_end_idx() - bucket_begin_idx(); }
+    const_iterator begin() const {
+      return map_->data_.begin() + bucket_begin_idx();
+    }
+    const_iterator end() const {
+      return map_->data_.begin() + bucket_end_idx();
+    }
+    friend iterator begin(bucket const& b) { return b.begin(); }
+    friend iterator end(bucket const& b) { return b.end(); }
+
+  private:
+    size_t bucket_begin_idx() const { return to_idx(map_->bucket_starts_[i_]); }
+    size_t bucket_end_idx() const {
+      return to_idx(map_->bucket_starts_[i_ + 1]);
+    }
+    bool is_inside_bucket(size_t const i) {
+      return bucket_begin_idx() + i < bucket_end_idx();
+    }
+
+    size_t const i_;
+    vecvec const* map_;
+  };
+
+  using value_type = bucket;
+  using const_value_type = const_bucket;
+
+  value_type operator[](index_value_type const i) { return {this, i}; }
+
+  const_value_type at(index_value_type const i) const {
+    utl::verify(to_idx(i) < bucket_starts_.size(),
+                "vecvec::at: index out of range");
+    return {this, i};
+  }
+
+  value_type at(index_value_type const i) {
+    utl::verify(to_idx(i) < bucket_starts_.size(),
+                "vecvec::at: index out of range");
+    return {this, i};
+  }
 
   size_t size() const { return empty() ? 0U : bucket_starts_.size() - 1; }
   bool empty() const { return data_.empty(); }
