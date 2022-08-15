16d81c43dd82bb90d10bb640c3a3607cf7d77009
Felix Gündling <felix.guendling@gmail.com>
2022-08-15 07:18:46 +0200

    flat_matrix(N) -> matrix(N, M)

---
+++ include/cista/containers/matrix.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "cista/containers/vector.h"
+
+namespace cista {
+
+template <typename Vector>
+struct base_matrix {
+  using value_type = typename Vector::value_type;
+  using size_type = typename Vector::size_type;
+
+  struct row {
+    row(base_matrix& matrix, int row_index)
+        : matrix_(matrix), row_index_(row_index) {}
+
+    value_type& operator[](int column_index) {
+      auto const pos = matrix_.column_count_ * row_index_ + column_index;
+      return matrix_.entries_[pos];
+    }
+
+    base_matrix& matrix_;
+    size_type row_index_;
+  };
+
+  struct const_row {
+    const_row(base_matrix const& matrix, int row_index)
+        : matrix_(matrix), row_index_(row_index) {}
+
+    value_type const& operator[](int column_index) const {
+      auto const pos = matrix_.column_count_ * row_index_ + column_index;
+      return matrix_.entries_[pos];
+    }
+
+    base_matrix const& matrix_;
+    size_type row_index_;
+  };
+
+  row operator[](size_type i) { return {*this, i}; }
+  const_row operator[](size_type i) const { return {*this, i}; }
+
+  value_type& operator()(size_type const i, size_type const j) {
+    return entries_[n_columns_ * i + j];
+  }
+
+  row at(size_type const i) {
+    verify(i < n_rows_, "matrix::at: index out of range");
+    return {*this, i};
+  }
+
+  const_row at(size_type const i) const {
+    verify(i < n_rows_, "matrix::at: index out of range");
+    return {*this, i};
+  }
+
+  void resize(size_type const n_rows, size_t const n_columns) {
+    entries_.resize(n_rows * n_columns);
+  }
+
+  void reset(value_type const& t) {
+    std::fill(begin(entries_), end(entries_), t);
+  }
+
+  size_type n_rows_{0U}, n_columns_{0U};
+  Vector entries_;
+};
+
+namespace offset {
+
+template <typename T>
+using matrix = base_matrix<vector<T>>;
+
+template <typename T>
+inline matrix<T> make_matrix(std::uint32_t const n_rows,
+                             std::uint32_t const n_columns,
+                             T const& init = T{}) {
+  auto v = vector<T>{};
+  v.resize(n_rows * n_columns, init);
+  return matrix<vector<T>>{n_rows, n_columns, std::move(v)};
+}
+
+}  // namespace offset
+
+namespace raw {
+
+template <typename T>
+using matrix = base_matrix<vector<T>>;
+
+template <typename T>
+inline matrix<T> make_matrix(std::uint32_t const n_rows,
+                             std::uint32_t const n_columns,
+                             T const& init = T{}) {
+  auto v = vector<T>{};
+  v.resize(n_rows * n_columns, init);
+  return matrix<vector<T>>{n_rows, n_columns, std::move(v)};
+}
+
+}  // namespace raw
+
+}  // namespace cista
