b19a3bd36a65ac5423c34b7e3e84e34c1701ac21
Felix Gündling <felix.guendling@gmail.com>
2023-05-17 09:47:19 +0200

    vecvec bucket push back and sort test

--- test/vecvec_test.cc
+++ test/vecvec_test.cc
@@ -34,3 +34,40 @@ TEST_CASE("vecvec string test") {
   d.emplace_back("hello");
   CHECK(d[key{0U}].view() == "hello");
 }
+
+TEST_CASE("vecvec sort bucket test") {
+  using key = cista::strong<unsigned, struct x_>;
+  using data = cista::raw::vecvec<key, char>;
+
+  data d;
+  d.emplace_back("hello");
+
+  std::sort(begin(d.at(key{0})), end(d.at(key{0})));
+
+  CHECK_EQ("ehllo", d.at(key{0}).view());
+}
+
+TEST_CASE("vecvec bucket emplace_back test") {
+  using key = cista::strong<unsigned, struct x_>;
+  using data = cista::raw::vecvec<key, char>;
+
+  data d;
+  d.emplace_back("hello");
+  d.emplace_back("world");
+  d.emplace_back("test");
+
+  d[key{0}].push_back('x');
+  CHECK_EQ("hellox", d[key{0}].view());
+  CHECK_EQ("world", d[key{1}].view());
+  CHECK_EQ("test", d[key{2}].view());
+
+  d[key{1}].push_back('x');
+  CHECK_EQ("hellox", d[key{0}].view());
+  CHECK_EQ("worldx", d[key{1}].view());
+  CHECK_EQ("test", d[key{2}].view());
+
+  d[key{2}].push_back('x');
+  CHECK_EQ("hellox", d[key{0}].view());
+  CHECK_EQ("worldx", d[key{1}].view());
+  CHECK_EQ("testx", d[key{2}].view());
+}
