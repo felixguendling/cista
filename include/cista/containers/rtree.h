2f73e6411fa8f1a14dd5f5f31aaccbf77a05a748
Alexander Wagner <alexanderdavidwagner@gmx.de>
2024-05-21 04:25:57 +0200

    Added documentation to existing functions in rtree.h

--- include/cista/containers/rtree.h
+++ include/cista/containers/rtree.h
@@ -34,14 +34,23 @@ struct rtree {
   using coord_t = array<NumType, Dims>;
 
   struct rect {
-    NumType united_area(rect const& o) const noexcept {
+    /**
+     * The area of the bounding rectangle of this rect and other_rect
+     * @param other_rect The second rectangle to calculate with
+     * @return The bounding area of this rect and other_rect
+     */
+    NumType united_area(rect const& other_rect) const noexcept {
       auto result = NumType{1};
       for (auto i = 0U; i != Dims; ++i) {
-        result *= (std::max(max_[i], o.max_[i]) - std::min(min_[i], o.min_[i]));
+        result *= (std::max(max_[i], other_rect.max_[i]) - std::min(min_[i], other_rect.min_[i]));
       }
       return result;
     }
 
+    /**
+     * Calculates the area of this rect
+     * @return The area of this rectangle
+     */
     NumType area() const noexcept {
       auto result = NumType{1};
       for (auto i = 0U; i != Dims; i++) {
@@ -50,40 +59,63 @@ struct rtree {
       return result;
     }
 
-    bool contains(rect const& o) const noexcept {
+    /**
+     * Checks if other_rect is contained in this rect
+     * @param other_rect The rectangle to check
+     * @return True if this rect contains other_rect
+     */
+    bool contains(rect const& other_rect) const noexcept {
       auto bits = 0U;
       for (auto i = 0U; i != Dims; i++) {
-        bits |= (o.min_[i] < min_[i]);
-        bits |= (o.max_[i] > max_[i]);
+        bits |= (other_rect.min_[i] < min_[i]);
+        bits |= (other_rect.max_[i] > max_[i]);
       }
       return bits == 0;
     }
 
-    bool intersects(rect const& o) const noexcept {
+    /**
+     * Checks if this rectangle intersects with other_rect
+     * @param other_rect The rectangle to check for intersection
+     * @return True if the rectangles intersect.
+     */
+    bool intersects(rect const& other_rect) const noexcept {
       auto bits = 0;
       for (auto i = 0U; i != Dims; i++) {
-        bits |= o.min_[i] > max_[i];
-        bits |= o.max_[i] < min_[i];
+        bits |= other_rect.min_[i] > max_[i];
+        bits |= other_rect.max_[i] < min_[i];
       }
       return bits == 0;
     }
 
-    bool onedge(rect const& o) const noexcept {
+    /**
+     * Checks if at least one edge of rect is touching other_rect
+     * @param other_rect The rectangle to check with
+     * @return True if the at least one of the edges of the rectangles are touching
+     */
+    bool onedge(rect const& other_rect) const noexcept {
       for (auto i = 0U; i < Dims; i++) {
-        if (feq(min_[i], o.min_[i]) || feq(max_[i], o.max_[i])) {
+        if (feq(min_[i], other_rect.min_[i]) || feq(max_[i], other_rect.max_[i])) {
           return true;
         }
       }
       return false;
     }
 
-    void expand(rect const& o) noexcept {
+    /**
+     * Expands the current rectangle by the coordinates of new_rect
+     * @param new_rect The rectangle to expand by
+     */
+    void expand(rect const& new_rect) noexcept {
       for (auto i = 0U; i < Dims; i++) {
-        min_[i] = std::min(min_[i], o.min_[i]);
-        max_[i] = std::max(max_[i], o.max_[i]);
+        min_[i] = std::min(min_[i], new_rect.min_[i]);
+        max_[i] = std::max(max_[i], new_rect.max_[i]);
       }
     }
 
+    /**
+     * Calculates the longest side of this rectangle and returns it's axis
+     * @return The axis of the longest side of the rectangle
+     */
     unsigned largest_axis() const noexcept {
       auto axis = 0U;
       auto nlength = max_[0] - min_[0];
@@ -101,51 +133,70 @@ struct rtree {
   };
 
   struct node {
+    /**
+     * Sorts rectangles in a node along one axis
+     * @param axis The axis to sort for
+     * @param rev Decides if sorted ascending or descending
+     * @param max If true sort with max coordinates else min coordinates
+     */
     void sort_by_axis(unsigned const axis, bool const rev, bool const max) {
       auto const by_index = max ? static_cast<unsigned>(Dims + axis) : axis;
       qsort(0U, count_, by_index, rev);
     }
 
-    void qsort(unsigned s, unsigned e, unsigned index, bool rev) {
+    /**
+     * Quicksort implementation for sorting rectangles (and its attached data) inside of a node
+     * @param start Start index of this partition
+     * @param end End index + 1 of this partition
+     * @param index Index for choosing the sorting axis of the rectangles
+     * @param rev Decides if sorted ascending or descending
+     */
+    void qsort(unsigned start, unsigned end, unsigned index, bool rev) {
+      // Simplification for the struct rect
       struct rect4 {
         NumType all[Dims * 2U];
       };
 
-      unsigned nrects = e - s;
+      unsigned nrects = end - start;
       if (nrects < 2) {
         return;
       }
       unsigned left = 0U;
       unsigned right = nrects - 1;
       unsigned pivot = nrects / 2;
-      swap(s + pivot, s + right);
-      auto const rects = reinterpret_cast<rect4 const*>(&rects_[s]);
+      swap(start + pivot, start + right);
+      auto const rects = reinterpret_cast<rect4 const*>(&rects_[start]);
       if (!rev) {
         for (auto i = 0U; i != nrects; ++i) {
           if (rects[i].all[index] < rects[right].all[index]) {
-            swap(s + i, s + left);
+            swap(start + i, start + left);
             ++left;
           }
         }
       } else {
         for (auto i = 0U; i != nrects; ++i) {
           if (rects[right].all[index] < rects[i].all[index]) {
-            swap(s + i, s + left);
+            swap(start + i, start + left);
             ++left;
           }
         }
       }
-      swap(s + left, s + right);
-      qsort(s, s + left, index, rev);
-      qsort(s + left + 1, e, index, rev);
+      swap(start + left, start + right);
+      qsort(start, start + left, index, rev);
+      qsort(start + left + 1, end, index, rev);
     }
 
-    unsigned choose_least_enlargement(rect const& ir) const {
+    /**
+     * Finds the index of the rect in this node with the least bounding area with insert_rect
+     * @param insert_rect The rectangle to check the bounding area with
+     * @return The index of th rect of this node with the least bounding area
+     */
+    unsigned choose_least_enlargement(rect const& insert_rect) const {
       auto j = 0U;
       auto j_enlarge = kInfinity;
       for (auto i = 0U; i < count_; i++) {
         // calculate the enlarged area
-        auto const uarea = rects_[i].united_area(ir);
+        auto const uarea = rects_[i].united_area(insert_rect);
         auto const area = rects_[i].area();
         auto const enlarge = uarea - area;
         if (enlarge < j_enlarge) {
@@ -156,20 +207,30 @@ struct rtree {
       return j;
     }
 
-    void move_rect_at_index_into(unsigned index, node& to) noexcept {
-      to.rects_[to.count_] = rects_[index];
+    /**
+     * Moves a rectangle and it's data from this node to into
+     * @param index The index of the rect to move
+     * @param into The node to move to
+     */
+    void move_rect_at_index_into(unsigned index, node& into) noexcept {
+      into.rects_[into.count_] = rects_[index];
       rects_[index] = rects_[count_ - 1];
       if (kind_ == kind::kLeaf) {
-        to.data_[to.count_] = data_[index];
+        into.data_[into.count_] = data_[index];
         data_[index] = data_[count_ - 1];
       } else {
-        to.children_[to.count_] = children_[index];
+        into.children_[into.count_] = children_[index];
         children_[index] = children_[count_ - 1U];
       }
       --count_;
-      ++to.count_;
+      ++into.count_;
     }
 
+    /**
+     * Swaps the rectangles at index i and j
+     * @param i First index
+     * @param j Second index
+     */
     void swap(unsigned i, unsigned j) noexcept {
       std::swap(rects_[i], rects_[j]);
       if (kind_ == kind::kLeaf) {
@@ -179,32 +240,30 @@ struct rtree {
       }
     }
 
+    /**
+     * Calculates the bounding box of the rectangles in this node
+     * @return The bounding rectangle of this node
+     */
     rect rect_calc() const noexcept {
       assert(count_ <= MaxItems);
-      auto r = rects_[0U];
+      auto temp_rect = rects_[0U];
       for (auto i = 1U; i < count_; ++i) {
-        r.expand(rects_[i]);
+        temp_rect.expand(rects_[i]);
       }
-      return r;
+      return temp_rect;
     }
 
     template <typename Ctx>
-    friend void serialize(Ctx& c, node const* origin,
-                          cista::offset_t const pos) {
+    friend void serialize(Ctx& c, node const* origin, cista::offset_t const pos) {
       using Type = std::decay_t<decltype(*origin)>;
-      c.write(pos + cista_member_offset(Type, count_),
-              convert_endian<Ctx::MODE>(origin->count_));
-      c.write(pos + cista_member_offset(Type, kind_),
-              convert_endian<Ctx::MODE>(origin->kind_));
-      c.write(pos + cista_member_offset(Type, rects_),
-              convert_endian<Ctx::MODE>(origin->rects_));
+      c.write(pos + cista_member_offset(Type, count_), convert_endian<Ctx::MODE>(origin->count_));
+      c.write(pos + cista_member_offset(Type, kind_), convert_endian<Ctx::MODE>(origin->kind_));
+      c.write(pos + cista_member_offset(Type, rects_), convert_endian<Ctx::MODE>(origin->rects_));
       if (origin->kind_ == kind::kLeaf) {
-        auto const size =
-            static_cast<offset_t>(sizeof(DataType) * origin->data_.size());
+        auto const size = static_cast<offset_t>(sizeof(DataType) * origin->data_.size());
         auto const data_start = pos + cista_member_offset(Type, data_);
         auto i = 0U;
-        for (auto it = data_start; it != data_start + size;
-             it += sizeof(DataType)) {
+        for (auto it = data_start; it != data_start + size; it += sizeof(DataType)) {
           serialize(c, &(origin->data_[i++]), it);
         }
       }
@@ -229,43 +288,58 @@ struct rtree {
     };
   };
 
+  /**
+   * Inserts a new rectangle with it's data into the data structure
+   * @param min Min coordinates of the new rectangle
+   * @param max Max coordinates of the new rectangle
+   * @param data The data to store
+   */
   void insert(coord_t const& min, coord_t const& max, DataType data) {
-    auto const r = rect{min, max};
+    auto const insert_rect = rect{min, max};
     while (true) {
       if (root_ == node_idx_t::invalid()) {
         root_ = node_new(kind::kLeaf);
-        rect_ = r;
+        rect_ = insert_rect;
         height_ = 1U;
       }
 
       auto split = false;
-      node_insert(rect_, root_, r, std::move(data), 0, split);
+      node_insert(rect_, root_, insert_rect, std::move(data), 0, split);
       if (!split) {
-        rect_.expand(r);
+        rect_.expand(insert_rect);
         ++count_;
         return;
       }
 
+      // Something goes wrong right here :(
       auto new_root_idx = node_new(kind::kBranch);
 
       auto right = node_idx_t::invalid();
       node_split(rect_, root_, right);
 
       auto new_root = get_node(new_root_idx);
-      assert(root_ < nodes_.size());
+      //assert(root_ < nodes_.size());
       new_root.rects_[0] = get_node(root_).rect_calc();
-      assert(right < nodes_.size());
+      //assert(right < nodes_.size());
       new_root.rects_[1] = get_node(right).rect_calc();
       new_root.children_[0] = root_;
       new_root.children_[1] = right;
-      new_root.count_ = 2;
       root_ = new_root_idx;
+      new_root.count_ = 2;
       ++height_;
     }
   }
 
-  void node_insert(rect const& nr, node_idx_t const n_idx, rect const& ir,
-                   DataType data, unsigned depth, bool& split) {
+  /**
+   * Inserts a new rectangle into a specified node or a node of its subtree
+   * @param nr NOT USED
+   * @param n_idx The id of the current node
+   * @param insert_rect The rectangle to insert
+   * @param data The data to insert
+   * @param depth The depth of the current node
+   * @param split True if this or a subsequent node had to be split
+   */
+  void node_insert(rect const& nr, node_idx_t const n_idx, rect const& insert_rect, DataType data, unsigned depth, bool& split) {
     auto& n0 = get_node(n_idx);
     if (n0.kind_ == kind::kLeaf) {
       if (n0.count_ == MaxItems) {
@@ -274,7 +348,7 @@ struct rtree {
       }
 
       auto const index = static_cast<unsigned>(n0.count_);
-      n0.rects_[index] = ir;
+      n0.rects_[index] = insert_rect;
       n0.data_[index] = std::move(data);
       n0.count_++;
       split = false;
@@ -282,10 +356,10 @@ struct rtree {
     }
 
     // Choose a subtree for inserting the rectangle.
-    auto const i = node_choose(n0, ir, depth);
-    node_insert(n0.rects_[i], n0.children_[i], ir, data, depth + 1U, split);
+    auto const i = node_choose(n0, insert_rect, depth);
+    node_insert(n0.rects_[i], n0.children_[i], insert_rect, data, depth + 1U, split);
     if (!split) {
-      get_node(n_idx).rects_[i].expand(ir);
+      get_node(n_idx).rects_[i].expand(insert_rect);
       return;
     }
 
@@ -297,12 +371,13 @@ struct rtree {
     node_idx_t right;
     node_split(get_node(n_idx).rects_[i], get_node(n_idx).children_[i], right);
 
+    // n1 should be replaceable with n0
     auto& n1 = get_node(n_idx);
     n1.rects_[i] = get_node(n1.children_[i]).rect_calc();
     n1.rects_[n1.count_] = get_node(right).rect_calc();
     n1.children_[n1.count_] = right;
     n1.count_++;
-    node_insert(nr, n_idx, ir, std::move(data), depth, split);
+    node_insert(nr, n_idx, insert_rect, std::move(data), depth, split);
   }
 
   void node_split(rect r, node_idx_t const n_idx, node_idx_t& right_out) {
@@ -364,12 +439,22 @@ struct rtree {
     return i;
   }
 
-  node& get_node(node_idx_t const n) { return nodes_[n]; }
+  /**
+   * Gets the node by id
+   * @param node_id Node id
+   * @return The corresponding node
+   */
+  node& get_node(node_idx_t const node_id) { return nodes_[node_id]; }
 
-  node_idx_t node_new(kind const k) {
-    auto& n = nodes_.emplace_back();
-    std::memset(&n, 0U, sizeof(node));
-    n.kind_ = k;
+  /**
+   * Creates a new node for the r_tree
+   * @param node_kind Specifies if leaf or branch node
+   * @return The id of the created node
+   */
+  node_idx_t node_new(kind const node_kind) {
+    auto& new_node = nodes_.emplace_back();
+    std::memset(&new_node, 0U, sizeof(node));
+    new_node.kind_ = node_kind;
     return node_idx_t{nodes_.size() - 1U};
   }
 
