bfef196ee1689dedcbc62b90bdec5034674eaf09
Felix Gündling <felix.guendling@gmail.com>
2024-08-31 20:47:42 +0200

    wrapped: comparison to nullptr, default ctor

--- include/cista/memory_holder.h
+++ include/cista/memory_holder.h
@@ -13,10 +13,15 @@ using memory_holder = std::variant<buf<mmap>, buffer, byte_buf>;
 
 template <typename T>
 struct wrapped {
+  wrapped() = default;
   wrapped(memory_holder mem, T* el) : mem_{std::move(mem)}, el_{el} {
     el_.self_allocated_ = false;
   }
   explicit wrapped(raw::unique_ptr<T> el) : el_{std::move(el)} {}
+
+  friend bool operator==(wrapped const& x, std::nullptr_t) {
+    return x.el_ == nullptr;
+  }
 
   T* get() const noexcept { return el_.get(); }
   T* operator->() noexcept { return el_.get(); }
