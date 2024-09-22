3241913bddf8c4202c48644466e8d1edf92755c5
Felix Gündling <felix.guendling@gmail.com>
2024-09-22 16:05:16 +0200

    enable vecvec::bucket::view generically

--- include/cista/containers/vecvec.h
+++ include/cista/containers/vecvec.h
@@ -33,9 +33,9 @@ struct basic_vecvec {
     data_value_type const* data() const { return empty() ? nullptr : &front(); }
 
     template <typename T = std::decay_t<data_value_type>,
-              typename = std::enable_if_t<std::is_same_v<T, char>>>
-    std::string_view view() const {
-      return std::string_view{begin(), size()};
+              typename Traits = std::char_traits<T>>
+    std::basic_string_view<T, Traits> view() const {
+      return std::basic_string_view<T, Traits>{begin(), size()};
     }
 
     value_type& front() {
