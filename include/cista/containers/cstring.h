80432687c9aaad98f9557fec745e2f13e2c62578
Ka Ho Ng <khng300@gmail.com>
2025-05-24 15:50:28 -0400

    cstring, vector: fix memory leaks
    For cstring:
    - reset() needs to be called when doing a move-assignment.
    
    For vector:
    - set() needs to call clear() to destroy the existing objects.

--- include/cista/containers/cstring.h
+++ include/cista/containers/cstring.h
@@ -117,6 +117,7 @@ struct generic_cstring {
   }
 
   void move_from(generic_cstring&& s) noexcept {
+    reset();
     std::memcpy(static_cast<void*>(this), &s, sizeof(*this));
     if constexpr (std::is_pointer_v<Ptr>) {
       std::memset(static_cast<void*>(&s), 0, sizeof(*this));
